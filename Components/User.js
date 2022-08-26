fb44cfea1fbb87dfa2ba6b6d0ee44bd7f1ff82e5
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-26 09:18:14 +0530

    Create User.js

---
+++ Components/User.js
@@ -0,0 +1,30 @@
+// function User(){ //example-01
+//     return(
+//         <h1>Hello Component</h1>
+//     )
+// }
+// export default User;
+
+// OR
+
+// export function User(){ //example-02 Function Component
+//     return(
+//         <h1>Hello Component</h1>
+//     )
+// }
+
+
+// example-03  // Class Componet
+
+// import React,{Component} from 'react'
+// export default class User extends Component //Or
+// // class User extends Component
+
+// {
+//     render(){
+//         return(
+//             <h1>Hello Class Component</h1>
+//         )
+//     }
+// }
+// // export default User
