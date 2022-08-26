1e131701eb1f39c870eb7e4b0852e7b5a2dc1df6
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-26 19:49:54 +0530

    Update App.js

--- Props/App.js
+++ Props/App.js
@@ -17,19 +17,51 @@
 
 // ### How to Change Data
 
-import React,{useState} from 'react'
+// import React,{useState} from 'react'
+// import './App.css';
+// import Student from './Student'
+// function App() {
+//   const [name,setName]=useState("Mane")
+//   return (
+//     <div className="App">
+//       <h1>Props in React :)</h1>
+//       <Student name={name} />
+//       <button onClick={()=>{setName("Vijay")}}>Update Name</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+//### Props Class Component
+
 import './App.css';
-import Student from './Student'
-function App() {
-  const [name,setName]=useState("Mane")
-  return (
-    <div className="App">
-      <h1>Props in React :)</h1>
-      <Student name={name} />
-      <button onClick={()=>{setName("Vijay")}}>Update Name</button>
-    </div>
-  );
+import Student from './Student';
+import React from 'react';
+class App extends React.Component{
+
+  constructor()
+  {
+    super();
+    this.state={
+      name:"VijayKumar"
+    }
+  }
+  render()
+  {
+    return(
+      <div className="App">
+        <h1>Props Class Compoent !</h1>
+        {/* <Student name="Kumar" email="kumar@gmail.com"></Student> */}
+
+        <Student name={this.state.name} email="vijay@gmail.com"></Student>
+        <button onClick={()=>this.setState({name:"Mane"})}>Update Name</button>    
+      </div>
+    )
+  }
 }
-
 export default App;
-
