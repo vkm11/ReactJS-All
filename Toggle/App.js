98d6e7968e2c13ecb5e0d7d89f9525a4d223918d
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-31 12:39:26 +0530

    Update App.js

--- Toggle/App.js
+++ Toggle/App.js
@@ -24,3 +24,32 @@ class App extends React.Component{
 
 
 export default App;
+
+
+
+{/* Example : 2. */}
+
+{/* 
+import './App.css';
+import React from 'react'
+function App(){
+   const [status, setStatus] = React.use state(true)
+   return(
+    <div className="App">
+     {
+       status?   
+       <h1>Hello World!</h1>
+       :null
+     }
+  //   <button onClick={()=>setStatus(false)}>Hide</button>
+  //   <button onClick={()=>setStatus(true)}>Show</button>
+     <button onClick={()=>setStatus(!status)}>Toggle</button>  
+    </div>
+   );
+}
+export default App;
+*/}
+
+
+
+
