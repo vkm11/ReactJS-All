16fc6ae109d1806236e1bba46960e2bb81d42f5b
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-31 17:35:55 +0530

    Update User.js

--- Life Cycle Methods/Render life cycle methods/User.js
+++ Life Cycle Methods/Render life cycle methods/User.js
@@ -5,7 +5,8 @@ class User extends React.Component {
      console.log("Render method", this.props)
      return (
        <div>
-          <h1>User Component</h1>
+          // <h1>User Component</h1>
+          <h1>User Component{this.props.name}</h1>
        </div>
      )
    }
