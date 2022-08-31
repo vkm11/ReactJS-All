24848f1fc942dfa2724a62947cb609cc507bcc41
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-31 16:25:04 +0530

    Create User.js

---
+++ Pass function as props/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function User(props)
+{
+  return (
+    <div className="App">
+      <h1> User Component</h1>
+      // <button onClick={()=>props.data()> Call data function</button>
+      <button onClick={props.data> Call data function</button>
+    </div>
+  );
+}
