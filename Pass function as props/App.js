9cbc022cc71ddfe957fec092700edbf35d76e2d3
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-08-31 16:27:39 +0530

    Update App.js

--- Pass function as props/App.js
+++ Pass function as props/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import User from './User'
 function App(){
+  function getData()
+  {
+    alert("hello from app")
   return(
     <div className="App">
       <h1> Pass function as props </h1>
+      <User data={getData} />
     </div>
   );
 }
