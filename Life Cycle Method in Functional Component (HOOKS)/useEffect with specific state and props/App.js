61e4a0a906740e1e0abc431ad707bb79f50a76f9
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-09-03 11:10:03 +0530

    Create App.js

---
+++ Life Cycle Method in Functional Component (HOOKS)/useEffect with specific state and props/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React,{useEffect, useState} from 'react'
+function App() {
+  const [data,setData]=useState(10);
+  const [count,setCount]=useState(100);
+
+  useEffect(()=>{
+    console.log("useEffect called")
+  },[data])// specific data inside call  needs to be used User.js
+  useEffect(()=>{
+    alert("Count is"+ count)
+  },[count])
+  return (
+    <div className="App">
+      <h1>useEffect with specific props and state</h1>
+      <h1>Count : {count}</h1>
+      <h1>Data : {data}</h1>
+      <button onClick={()=>setCount(count+1) }>Update Count</button>
+      <button onClick={()=>setData(data+1) }>Update Data</button>
+    </div>
+  );
+}
+
+export default App;
