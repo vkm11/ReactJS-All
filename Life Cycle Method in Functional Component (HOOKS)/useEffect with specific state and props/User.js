3d4ffc7f6ab837f8e5cd1796eafcf4ed6d6a56e3
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-09-03 11:31:50 +0530

    Update User.js

--- Life Cycle Method in Functional Component (HOOKS)/useEffect with specific state and props/User.js
+++ Life Cycle Method in Functional Component (HOOKS)/useEffect with specific state and props/User.js
@@ -1,20 +1,16 @@
-// Example: 2
+// Example 2
 
 import './App.css';
 import React,{useEffect, useState} from 'react'
-function App() {
-  const [data,setData]=useState(10);
-  const [count,setCount]=useState(100);
-
+function App(props) {
   useEffect(()=>{
     console.log("useEffect called")
-  },[data])// specific data inside call  needs to be used User.js
+  })// specific data inside call  needs to be used User.js
 
   return (
     <div className="App">
-      <h1>useEffect with specific props and state</h1>
-      <h1>Count : {count}</h1>
-      <h1>Data : {data}</h1>
+      <h1>Count Props : {props.count}</h1>
+      <h1>Data Props: {props.data}</h1>
     </div>
   );
 }
