5c39ea422f24bff198698fd2c199474da9866b15
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-09-05 17:32:16 +0530

    Update App.js

--- Array Listing with map/App.js
+++ Array Listing with map/App.js
@@ -1,21 +1,38 @@
 import './App.css';
 
-const students=["vijay","kumar","Vijaykumar", "Mane"];
+// const students=["vijay","kumar","Vijaykumar", "Mane"];
 
-// map looping
-students.map((item)=>{
-  console.log("my Name is:",item)
-});
+const students=[
+  { name:"vijay", email:"vijay@gmail.com", contact_No:"8930723479"},
+  { name:"kumar", email:"kumar@gmail.com", contact_No:"893345345"},
+  { name:"vijaykumar", email:"vijaykumar@gmail.com", contact_No:"8930235673"},
+]
 
-// for loop
-for(let i=0; i<students.length;i++)
-{
-  console.log("in for loop my name is:",students[i])
-}
 function App() {
   return (
     <div className="App">
-     <h1>ArrayList With Map</h1>
+     <h1>Handle Array with List</h1>
+
+     {/* Map */} 
+     
+     <table border="1">
+     <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contact_No</td>
+      </tr>
+     {
+      students.map((data)=>
+        <tr>
+          <td>{data.name}</td>
+          <td>{data.email}</td>
+          <td>{data.contact_No}</td>
+        </tr>
+      )
+     }
+
+     </table>
+    
     </div>
   );
 }
