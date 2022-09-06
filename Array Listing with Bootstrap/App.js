f0fdb9c1e8595492ae070cfe6a91d638c2a7c317
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-09-06 10:24:48 +0530

    Update App.js

--- Array Listing with Bootstrap/App.js
+++ Array Listing with Bootstrap/App.js
@@ -1,67 +1,36 @@
 import './App.css';
 import React from 'react'
-import {Table} from 'react-bootstrap'
-
-function App()
-{
-  const users = [
-    { name: 'Vijay', email: 'vijay@gmail.com', 
-      address:[
-        { Hn:'110',city:'sadalga', country:'india'},
-        { Hn:'120',city:'bedkihal', country:'india'},
-        { Hn:'130',city:'belagavi', country:'india'},
-        { Hn:'140',city:'bangalore', country:'india'}
-      ]
-    },
-    
-    { name: 'Kumar', email: 'kumar@gmail.com', 
-    address:[
-       { Hn:'101',city:'Janawad', country:'india'},
-       { Hn:'102',city:'Chikodi', country:'india'},
-       { Hn:'103',city:'Nipani', country:'india'},
-       { Hn:'104',city:'Belgaum', country:'india'}
-      ]
-    },
-  ]
-  return(
+import{Table} from 'react-bootstrap'
+const users=[
+  { name:"vijay", email:"vijay@gmail.com", contact_No:"8930723479"},
+  { name:"kumar", email:"kumar@gmail.com", contact_No:"893345345"},
+  { name:"vijaykumar", email:"vijaykumar@gmail.com", contact_No:"8930235673"},
+]
+function App() {
+  return (
     <div className="App">
-      <h1>List with nested array</h1>
-        <Table variant="black" striped>
-          <tbody>
-            <tr>
-              <td>S.N</td>
-              <td>Name</td>
-              <td>Email</td>
-              <td>Address</td>
-            </tr>
-          </tbody>
-          {
-            users.map((item,i)=>
-            <tr key={i}>
-              <td>{i+1}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>
-                <Table variant="black" striped>
-                  <tbody>
-                  {
-                    item.address.map((data)=>
-                    <tr>
-                      <td>{data.Hn}</td>
-                      <td>{data.city}</td>
-                      <td>{data.country}</td>
-                    </tr>
-                    )
-                  }
-                  </tbody>
-                </Table>
-               
-              </td>
-            </tr>
-            )
-          }
-        </Table>
+      <h1>Bootstrap Table</h1>
+      <Table striped variant='dark'>
+        <tbody>
+      <tr>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Contact</td>
+      </tr>
+      {
+        users.map((item, i)=>
+        item.contact ==='111'?
+        <tr key={i}>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.contact_No}</td>
+        </tr>:null
+        )
+      }
+      </tbody>
+      </Table>
     </div>
   );
 }
+
 export default App;
